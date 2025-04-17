8deb250d67e5627ad3118b3daacbc3fd013e5460
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2025-04-18 00:25:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -102,4 +102,4 @@ if __name__ == "__main__":
     util.load_saved_artifacts()
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
