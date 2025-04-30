02f56f19d5e4d387ed18fd23305937b3b05bf458
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2025-04-30 14:50:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -122,6 +122,14 @@ def get_location_names_api():
     locations = get_location_names()
     return jsonify({'locations': locations})
 
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = 'https://hardikkapoor27.github.io'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS']
+    return response
+
 # ----------- Run the App -----------
 if __name__ == '__main__':
     app.run(debug=True)
