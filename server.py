14c9b1ba65d72bd42123eba55524f2fa3a0cf807
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2025-04-30 15:43:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 import sqlite3
 import hashlib
-SameSite=None; Secure=true
 from util import get_estimated_price, get_location_names, load_saved_artifacts
 
 app = Flask(__name__)
@@ -93,7 +92,7 @@ def after_request(response):
     response.headers['Access-Control-Allow-Origin'] = 'https://hardikkapoor27.github.io'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS']
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     return response
     
 # ----------- Prediction & History Routes -----------
